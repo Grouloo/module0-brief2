7da0d30eae65acf3d70eb4db740f572330d26ab0
Elliott Lépine <elliott@brad.ag>
2025-11-04 23:16:35 +0100

    Add image generation prompt engineering with a language model

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ import streamlit as st
 import requests
 from loguru import logger
 from sys import stderr
-from transformers import pipeline
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 import torch
 import torchaudio
 import io
 
-pipe = pipeline("automatic-speech-recognition", model="openai/whisper-small", language='fr')
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+speech_to_text = pipeline("automatic-speech-recognition", model="openai/whisper-small", language='fr')
+
+# Load Qwen model and tokenizer
+model_name = "Qwen/Qwen3-0.6B"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+text_to_prompt_model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype=torch.float16,
+)
 
 logger.add(stderr, format="{time} {level} {message}", filter="my_module", level="INFO")
 logger.add("logs/main.log")
@@ -24,9 +37,44 @@ with st.form("my_form"):
             audio_buffer = io.BytesIO(audio_bytes)
             audio_tensor, sample_rate = torchaudio.load(audio_buffer)
 
-            text_output = pipe(audio_tensor)
+            text_output = speech_to_text(audio_tensor)
 
-            st.text(text_output)
+            st.text(f"Transcription : {text_output['text']}")
+
+            # Prepare the model input
+            messages = [
+                {"role": "user", 
+                "content": 
+                f"Transforme cette description en prompt Midjourney optimisée. Format de sortie : [sujet principal], [style artistique], [détails visuels], [éclairage], [ambiance], [paramètres techniques]\n Description : {text_output['text']}"}
+            ]
+            
+            text = tokenizer.apply_chat_template(
+                messages,
+                tokenize=False,
+                add_generation_prompt=True,
+                enable_thinking=False
+            )
+            
+            model_inputs = tokenizer([text], return_tensors="pt").to(text_to_prompt_model.device)
+            
+            # Generate text
+            generated_ids = text_to_prompt_model.generate(
+                **model_inputs,
+                max_new_tokens=32768
+            )
+            
+            output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
+
+            try:
+                # rindex finding 151668 (</think>)
+                index = len(output_ids) - output_ids[::-1].index(151668)
+            except ValueError:
+                index = 0
+
+            prompt =tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
+
+            st.text(f"Prompt: {prompt}")
+
         except requests.exceptions.HTTPError as e:
             st.error(f"Erreur HTTP : {e}")
             logger.error(f"Erreur HTTP : {e}")
--- readme.md
+++ readme.md
@@ -19,3 +19,21 @@ streamlit run main.py
 Un nouvel onglet devrait s'ouvrir.
 
 Si ce n'est pas le cas, l'application devrait tout de même être accessible depuis l'adresse http://localhost:8501/
+
+## Défis techniques
+
+### Choix du modèle de langage
+
+Plusieurs difficultés ont été rencontrées lors de la réalisation du segment de l'application permettant de transformer la transcription du message de l'utilisateur en véritable prompt pour un modèle de génération d'image.
+
+Il fallait dans un premier temps choisir un modèle ni trop gros (pour qu'il puisse tourner en local sur une machine personnelle) ni trop petit (au risque de se retrouver avec une réponse sans queue ni tête, comme avec GPT-2 par exemple)
+
+Le modèle utilisé ici est finalement `Qwen3-0.6B`, avec son étape de réflexion désactivée afin de raccourcir le temps de traitement (le contenu produit durant l'étape de réflexion lors de cette tâche n'était de toute façon pas très pertinent : le modèle tournait en rond et ne parvenait pas à aboutir à quelque chose de concret).
+
+La deuxième étape fut de trouver le bon prompt : en effet, le modèle étant assez petit, il faut pouvoir lui communiquer des instructions compréhensibles, structurées, et courtes.
+
+Plusieurs prompts ont été essayé, mais dans la plupart des cas le modèle n'arrivait simplement pas à donner une réponse avec la structure attendue ou bien était hors-sujet, quand il n'était pas carrément en train de simplement répéter la description textuelle donnée.
+
+Le prompt utilisé actuellement est celui-ci : "Transforme cette description en prompt Midjourney optimisée. Format de sortie : [sujet principal], [style artistique], [détails visuels], [éclairage], [ambiance], [paramètres techniques]\n Description : {transcription du message audio}".
+
+Il semble fonctionner correctement dans la plupart des cas, mais nul doute qu'il pourrait encore être amélioré.
